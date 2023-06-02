7271969ed61e30a4a8347d5cea20d91976b61f46
Taufik-H <taufikrpl1@gmail.com>
2023-06-02 16:26:17 +0700

    first commit

---
+++ app/src/main/java/project/greetiny/auth/SignInActivity.java
@@ -0,0 +1,17 @@
+package project.greetiny.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import project.greetiny.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+}
---
+++ app/src/main/java/project/greetiny/auth/SignUpActivity.java
@@ -0,0 +1,16 @@
+package project.greetiny.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import project.greetiny.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
