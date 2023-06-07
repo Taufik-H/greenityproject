a54c9f9739ea3c71845e78b5febf692bc2d35463
AzizSetiawan <aziz_setiawan13579@yahoo.co.id>
2023-06-08 00:30:59 +0700

    njiot button kartu

--- app/src/main/java/project/greetiny/ucapan/data_ulangTahun.java
+++ app/src/main/java/project/greetiny/ucapan/data_ulangTahun.java
@@ -1,2 +1,66 @@
-package project.greetiny.ucapan;public class data_ulangTahun {
+package project.greetiny.ucapan;
+
+public class data_ulangTahun {
+    private String gambar;
+    private String subject;
+    private String object;
+    private String tanggal;
+    private String ucapan;
+    private String key;
+
+    public data_ulangTahun() {
+        this.gambar = gambar;
+        this.subject = subject;
+        this.object = object;
+        this.tanggal = tanggal;
+        this.ucapan = ucapan;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getUcapan() {
+        return ucapan;
+    }
+
+    public void setUcapan(String ucapan) {
+        this.ucapan = ucapan;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 }
