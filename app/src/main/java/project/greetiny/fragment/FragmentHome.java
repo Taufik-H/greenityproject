6644fd6d7bcd0daeee5ea242c2fed5617c001a16
Taufik-H <taufikrpl1@gmail.com>
2023-06-08 01:03:45 +0700

    abcd

--- app/src/main/java/project/greetiny/fragment/FragmentHome.java
+++ app/src/main/java/project/greetiny/fragment/FragmentHome.java
@@ -10,10 +10,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import project.greetiny.MainActivity;
 import project.greetiny.R;
+import project.greetiny.auth.SignInActivity;
 import project.greetiny.ucapan.UlangTahun;
 
 /**
@@ -24,18 +29,6 @@ import project.greetiny.ucapan.UlangTahun;
 public class FragmentHome extends Fragment {
     private MaterialCardView card1;
 
-
-
-
-
-
-
-
-
-
-
-    //default code
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -44,20 +37,15 @@ public class FragmentHome extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseAuth mAuth;
+
+    private ImageView profile;
+    private RelativeLayout includedLayout;
 
     public FragmentHome() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentHome.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentHome newInstance(String param1, String param2) {
         FragmentHome fragment = new FragmentHome();
         Bundle args = new Bundle();
@@ -75,11 +63,19 @@ public class FragmentHome extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
+
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        //card 1 ulang tahun
         card1 = view.findViewById(R.id.card1);
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
