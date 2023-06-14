f024209a884ef9bca7e1deba5afda4c4dbbb7103
IkhsanDwii <ikhsanjunior708@gmail.com>
2023-06-15 01:04:40 +0700

    loti buton

---
+++ app/src/main/java/project/greetiny/adapter/myadapter.java
@@ -0,0 +1,64 @@
+package project.greetiny.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import project.greetiny.R;
+import project.greetiny.fragment.FragmentList;
+
+
+public class myadapter extends FirebaseRecyclerAdapter<model,myadapter.myviewholder> {
+
+    public myadapter(@NonNull FirebaseRecyclerOptions<model> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull model model) {
+        holder.nametext.setText(model.getSubject());
+
+        holder.img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatActivity activity=(AppCompatActivity)view.getContext();
+                activity.getSupportFragmentManager().beginTransaction().replace
+                                (R.id.recycleview,new FragmentList(model.getSubject()))
+                        .addToBackStack(null).commit();
+            }
+        });
+
+    }
+
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new myviewholder(view);
+    }
+
+    public class myviewholder extends RecyclerView.ViewHolder {
+        ImageView img1;
+        TextView nametext;
+
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            nametext = itemView.findViewById(R.id.name);
+        }
+    }
+}
+
+
+
--- app/src/main/java/project/greetiny/fragment/FragmentList.java
+++ app/src/main/java/project/greetiny/fragment/FragmentList.java
@@ -2,15 +2,22 @@ package project.greetiny.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
@@ -18,41 +25,29 @@ import java.util.List;
 import project.greetiny.R;
 import project.greetiny.adapter.DataClass;
 import project.greetiny.databinding.ActivityMainBinding;
+import project.greetiny.adapter.model;
+import project.greetiny.adapter.myadapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentList#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragmentList extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    String name;
+    myadapter adapter;
+    public FragmentList() {
+    }
+
     RecyclerView recyclerView;
     List<DataClass> dataList;
     DatabaseReference databaseReference;
     ValueEventListener eventListener;
     private ActivityMainBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public FragmentList() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentList.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentList newInstance(String param1, String param2) {
         FragmentList fragment = new FragmentList();
         Bundle args = new Bundle();
@@ -60,6 +55,11 @@ public class FragmentList extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public FragmentList(String subject) {
+        this.name=subject;
+
     }
 
     @Override
@@ -75,9 +75,26 @@ public class FragmentList extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        recyclerView=(RecyclerView)view.findViewById(R.id.recycleview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        FirebaseRecyclerOptions<model> options =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("kartu"), model.class)
+                        .build();
 
+        adapter=new myadapter(options);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    public void onBackPressed()
+    {
+        AppCompatActivity activity=(AppCompatActivity)getContext();
+        activity.getSupportFragmentManager().beginTransaction()
+                .replace(R.id.recycleview,new FragmentList()).addToBackStack(null).commit();
 
     }
 
